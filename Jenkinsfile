01ae9745e85b57eaa66358071de231e09ac6f9ed
= <=>
2021-05-02 11:42:37 +1200

    tweaked scripts for permissions

--- Jenkinsfile
+++ Jenkinsfile
@@ -16,6 +16,7 @@ pipeline {
         }
         stage('Test') {
             steps {
+                sh 'chmod +x ./jenkins/test.sh'
                 sh './jenkins/scripts/test.sh'
             }
         }
--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -3,5 +3,5 @@
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
-set -x
+#set -x
 kill $(cat .pidfile)
